12941c65b6f5dc09d329749681cca40218becafb
unknown <icbt@NG-LAB01-LECHPC.student.icbt.local>
2023-07-16 15:15:10 +0530

    Include test project

---
+++ tharik/test-proj/src/main/java/com/mycompany/test/proj/Calculator.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.test.proj;
+
+/**
+ *
+ * @author icbt
+ */
+public class Calculator {
+    public int add(int no1, int no2) {
+        return no1 + no2;
+    }
+    
+    public int subtract(int no1, int no2) {
+        return no1 - no2;
+    }
+    
+    public int divide(int no1, int no2) {
+        return no1 / no2;
+    }
+        
+        
+    public int multiplied(int no1, int no2) {
+        return no1 * no2;
+    }
+}
